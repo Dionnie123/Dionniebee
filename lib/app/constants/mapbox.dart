fdc780c625b41d25237ff9bbdedc1d6e9bea25b0
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-08-19 10:40:58 +0800

    up

---
+++ lib/app/constants/mapbox.dart
@@ -0,0 +1,8 @@
+const mpUrlTemplate =
+    'https://api.mapbox.com/styles/v1/mbulingit/cll89rse000b401puaxxlcroa/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWJ1bGluZ2l0IiwiYSI6ImNsbDdqdG1ucTBzd2Izc3FxYXF2Nnp3NXMifQ.C0RXDFn0tmETYAAhKrITDw';
+
+const mpAdditionOption = {
+  'token':
+      'pk.eyJ1IjoibWJ1bGluZ2l0IiwiYSI6ImNsbDdqdG1ucTBzd2Izc3FxYXF2Nnp3NXMifQ.C0RXDFn0tmETYAAhKrITDw',
+  'id': 'mapbox.mapbox-streets-v8'
+};
--- lib/ui/widgets/common/my_map/widgets/animated_map.dart
+++ lib/ui/widgets/common/my_map/widgets/animated_map.dart
@@ -1,11 +1,24 @@
+import 'package:dionniebee/app/constants/mapbox.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart';
+
+import 'map_floating_action_buttons.dart';
 
 class AnimatedMap extends StatefulWidget {
+  final LatLngBounds? boundary;
+  final List<Marker> markers;
+  final LatLng? currentPoint;
   final Function() onPop;
-  final Widget Function(BuildContext context, AnimatedMapController controller)
-      builder;
-  const AnimatedMap({super.key, required this.builder, required this.onPop});
+
+  const AnimatedMap(
+      {super.key,
+      required this.onPop,
+      required this.markers,
+      this.boundary,
+      this.currentPoint});
 
   @override
   State<AnimatedMap> createState() => _AnimatedMapState();
@@ -13,17 +26,206 @@ class AnimatedMap extends StatefulWidget {
 
 class _AnimatedMapState extends State<AnimatedMap>
     with TickerProviderStateMixin {
-  late final _animatedMapController = AnimatedMapController(vsync: this);
+  LatLng? dragPoint;
+
+  AnimatedMapController? _animatedMapController;
+  MapController? _mapController;
+  bool mapReady = false;
+
+  updatePointOnDrag() {
+    setState(() {
+      dragPoint = _animatedMapController?.mapController.center;
+    });
+  }
+
+  @override
+  void initState() {
+    try {
+      _animatedMapController = AnimatedMapController(vsync: this);
+      _mapController = _animatedMapController?.mapController;
+
+      setState(() {
+        if (_animatedMapController != null &&
+            _mapController != null &&
+            widget.currentPoint != null) {}
+        mapReady = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+
+    super.initState();
+  }
 
   @override
   void dispose() {
     () => widget.onPop();
-    _animatedMapController.dispose();
+    _animatedMapController?.dispose();
     super.dispose();
   }
 
+  Widget mapTemplate() {
+    return TileLayer(
+      urlTemplate: mpUrlTemplate,
+      additionalOptions: mpAdditionOption,
+    );
+  }
+
+  Widget attributeLayer() {
+    return const RichAttributionWidget(
+      attributions: [
+        TextSourceAttribution(
+          'OpenStreetMap contributors',
+          /*       onTap: () => launchUrl(
+                        Uri.parse('https://openstreetmap.org/copyright')), */
+        ),
+      ],
+    );
+  }
+
+  Widget clusteredMapWidget() {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20), color: Colors.red),
+      child: Center(
+        child: Text(
+          widget.markers.length.toString(),
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Marker currentPointMarker() {
+    return Marker(
+        point: widget.currentPoint ?? const LatLng(0, 0),
+        builder: (context) {
+          return const Icon(
+            Icons.location_pin,
+            size: 35,
+            color: Colors.pink,
+          );
+        });
+  }
+
+  Widget circle() {
+    return (widget.currentPoint == null)
+        ? const SizedBox.shrink()
+        : CircleLayer(circles: [
+            CircleMarker(
+              borderColor: Colors.red,
+              borderStrokeWidth: 2,
+              color: Colors.orange.withOpacity(0.1),
+              point: widget.currentPoint ?? const LatLng(0, 0),
+              radius: 15000,
+              useRadiusInMeter: true,
+            )
+          ]);
+  }
+
+  final philippineMaxBounds = LatLngBounds.fromPoints(
+      [const LatLng(4.382696, 112.1661), const LatLng(21.53021, 127.0742)]);
+
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, _animatedMapController);
+    return !mapReady
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : LayoutBuilder(builder: (context, size) {
+            return Stack(
+              children: [
+                widget.currentPoint == null
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : FlutterMap(
+                        mapController: _animatedMapController?.mapController,
+                        options: MapOptions(
+                          onMapReady: () {
+                            updatePointOnDrag();
+                          },
+                          onMapEvent: (event) {
+                            updatePointOnDrag();
+                          },
+                          interactiveFlags: InteractiveFlag.drag |
+                              InteractiveFlag.flingAnimation |
+                              InteractiveFlag.pinchMove |
+                              InteractiveFlag.pinchZoom |
+                              InteractiveFlag.doubleTapZoom,
+                          boundsOptions: const FitBoundsOptions(
+                              forceIntegerZoomLevel: true, inside: true),
+                          maxBounds: philippineMaxBounds,
+                          center: widget.currentPoint,
+                          rotationThreshold: 0.0,
+                          zoom: 12.0,
+                        ),
+                        nonRotatedChildren: [attributeLayer()],
+                        children: [
+                          mapTemplate(),
+                          circle(),
+                          MarkerLayer(
+                            markers: [
+                              currentPointMarker(),
+                            ],
+                          ),
+                          MarkerClusterLayerWidget(
+                            options: MarkerClusterLayerOptions(
+                              anchorPos: AnchorPos.align(AnchorAlign.center),
+                              maxClusterRadius: 100,
+                              size: const Size(40, 40),
+                              fitBoundsOptions: const FitBoundsOptions(
+                                //Pag-tap pakita agad ng branch
+                                forceIntegerZoomLevel: false,
+                                padding: EdgeInsets.all(50),
+                              ),
+                              markers: [
+                                ...widget.markers,
+                              ],
+                              builder: (context, markers) {
+                                return clusteredMapWidget();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: MapFloatActionButtons(
+                          controller: _animatedMapController,
+                          centerPoint:
+                              widget.currentPoint ?? const LatLng(0, 0),
+                        ))),
+                /*   Positioned(
+                  top: (size.maxHeight - 35) / 2,
+                  left: (size.maxWidth / 2) - 35 / 2,
+                  child: const Icon(
+                    Icons.location_pin,
+                    size: 35,
+                    color: Colors.green,
+                  ),
+                ), */
+                Align(
+                  child: Column(
+                    children: [
+                      Text(widget.currentPoint.toString()),
+                      Text(dragPoint.toString()),
+                    ],
+                  ),
+                ),
+                Align(
+                  child: Column(
+                    children: [
+                      Text(widget.currentPoint.toString()),
+                      Text(dragPoint.toString()),
+                    ],
+                  ),
+                )
+              ],
+            );
+          });
   }
 }
