7c599b3affeb318e07c49730999cb6c3c0088f10
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-09-18 09:12:20 +0800

    up

--- lib/ui/views/stores/stores_view.dart
+++ lib/ui/views/stores/stores_view.dart
@@ -1,27 +1,34 @@
-import 'package:dionniebee/app/models/location_dto.dart';
 import 'package:dionniebee/ui/common/ui_helpers.dart';
+import 'package:dionniebee/ui/views/stores/widgets/delivery_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_map_animations/flutter_map_animations.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:stacked/stacked.dart';
 import 'stores_viewmodel.dart';
-import 'widgets/map_widget.dart';
-import 'widgets/sliding_up_panel_list.dart';
+import 'widgets/pickup_view.dart';
 
-class StoresView extends HookWidget {
+class StoresView extends StatefulWidget {
   const StoresView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final a = useSingleTickerProvider();
-    final b = useSingleTickerProvider();
-    final AnimatedMapController animatedMapController = AnimatedMapController(
-      vsync: a,
-    );
-    final TabController tabController =
-        TabController(initialIndex: 0, length: 2, vsync: b);
+  State<StoresView> createState() => _StoresViewState();
+}
 
+class _StoresViewState extends State<StoresView> with TickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ViewModelBuilder<StoresViewModel>.reactive(
         viewModelBuilder: () => StoresViewModel(),
         initialiseSpecialViewModelsOnce: true,
@@ -94,78 +101,9 @@ class StoresView extends HookWidget {
                 Expanded(
                   child: TabBarView(
                     controller: tabController,
-                    children: [
-                      Scaffold(
-                        appBar: AppBar(
-                            backgroundColor: Colors.white,
-                            automaticallyImplyLeading: false,
-                            titleSpacing: 8,
-                            title: Container(
-                              width: double.infinity,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Center(
-                                      child: TextField(
-                                        controller: viewModel.textController,
-                                        decoration: InputDecoration(
-                                            filled: true,
-                                            suffixIcon: IconButton(
-                                              icon: const Icon(Icons
-                                                  .center_focus_weak_rounded),
-                                              onPressed: () async {
-                                                await viewModel.start();
-                                              },
-                                            ),
-                                            hintText: 'Search...',
-                                            border: InputBorder.none),
-                                      ),
-                                    ),
-                                  ),
-                                  hSpaceSmall,
-                                  ElevatedButton(
-                                      onPressed: () {},
-                                      child: const Text("Filters")),
-                                ],
-                              ),
-                            )),
-                        body: Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 200),
-                              child: MapWidget(
-                                mapController: animatedMapController,
-                                isBusy: viewModel.busy(loaderBusy),
-                                isMapReloading: viewModel.busy(mapBusy),
-                                center: viewModel.locationNonStreamValue ??
-                                    viewModel.calculateCenterPoint(),
-                                markers: viewModel.markers,
-                                onPositionChanged: (event, point) {
-                                  viewModel.mapInfo = LocationDto(
-                                    maxDistance: 1000,
-                                    geopoint: LatLngDto(
-                                      latitude: event.center?.latitude,
-                                      longitude: event.center?.longitude,
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                            SlidingUpPanel(
-                              // color: Colors.black.withOpacity(0.8),
-                              backdropEnabled: true,
-                              minHeight: 200,
-                              panel: SlidingUpPanelList(
-                                  items: viewModel.nearbyLocations),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Text("DELIVERY")
+                    children: const [
+                      DeliveryView(),
+                      PickupView(),
                     ],
                   ),
                 ),
@@ -173,14 +111,5 @@ class StoresView extends HookWidget {
             ),
           );
         });
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
